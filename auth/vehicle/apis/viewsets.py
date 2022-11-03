e2b74f0c7f23baa80dbc391fad6b0d192895bdb0
abhishek <abhishekdwivediofficial@gmail.com>
2022-11-03 18:53:06 +0530

    changes in vehicle

--- auth/vehicle/apis/viewsets.py
+++ auth/vehicle/apis/viewsets.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from auth.permissions import AccessControlPermission
 from .serializers import VehicleSerializer
 from vehicle.models import Vehicle
@@ -11,3 +11,11 @@ class VehicleViewSet(viewsets.ModelViewSet):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
     # permission_classes = [AccessControlPermission]
+
+
+
+    def get_queryset(self):
+        payload = self.request.auth.payload
+        organization_id = payload['organization_id']
+        qs = self.queryset.filter(organization__uuid=organization_id)
+        return qs
