9050fc0ed80be5074dd68360cffb0438ff3ce522
abhishek <abhishekdwivediofficial@gmail.com>
2022-11-03 18:53:06 +0530

    changes in vehicle

--- auth/vehicle/apis/serializers.py
+++ auth/vehicle/apis/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from vehicle.models import Vehicle
+from vehicle.apis.serializers import VehicleSerializer
+from organization.apis.serializers import OrganizationSerializer
 from rest_framework.reverse import reverse
 
 
 class VehicleSerializer(serializers.ModelSerializer):
-    id = serializers.ReadOnlyField(source="uuid")
-
+    id = serializers.ReadOnlyField(source='uuid')
+    vehicle_type = serializers.ReadOnlyField
+    organization = OrganizationSerializer(read_only=True)
     class Meta:
         model = Vehicle
         fields = (
@@ -13,6 +16,4 @@ class VehicleSerializer(serializers.ModelSerializer):
             'make',
             'model',
             'vin',
-            
-
         )
