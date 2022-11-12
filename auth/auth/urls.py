2daf7194f9dcf029bf0f9308f912fe480b768fc6
Avinash Mishra <avinashmishra1904@gmail.com>
2022-11-12 19:58:39 +0530

    added features list in user and login response

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     path("auth/admin/", admin.site.urls),
     path("auth/api/v1/", include("auth.apiurls.auth_apiurls")),
     path("transport/api/v1/", include("auth.apiurls.transport_apiurls")),
-    # path('transport/api/v1/', include('auth.apiurls'))
 ]
