c8069c63f8c18b68068f246d5d1897d1ee5680db
Avinash Mishra <avinashmishra1904@gmail.com>
2022-11-25 12:29:01 +0530

    fixed creation of drivers

--- auth/driver/apis/viewsets.py
+++ auth/driver/apis/viewsets.py
@@ -2,19 +2,12 @@ from rest_framework import viewsets
 from .serializers import DriverSerializer
 from driver.models import Driver
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from auth.viewsets import BaseViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
 
-class DriverViewSet(viewsets.ModelViewSet):
+class DriverViewSet(BaseViewSet):
     lookup_field = "uuid"
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
-
-    def get_queryset(self):
-        payload = self.request.auth.payload
-        JWTA = JWTAuthentication()
-        user = JWTA.get_user(payload)
-        if user.is_superuser:
-            return self.queryset
-        organization_id = payload["organization_id"]
-        qs = self.queryset.filter(organization__uuid=organization_id)
-        return qs
