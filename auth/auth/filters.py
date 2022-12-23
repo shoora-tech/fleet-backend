661cc7ba20d20e8e899e890717fe4a164befb2b9
Avinash Mishra <avinashmishra1904@gmail.com>
2022-12-23 07:41:37 +0530

    added device_id filters

--- auth/auth/filters.py
+++ auth/auth/filters.py
@@ -1,12 +1,22 @@
 from django_filters import rest_framework as filters
 from alert.models import RealTimeDatabase, Alert
+from vehicle.models import Vehicle
 
 class RealtimeDBFilter(filters.FilterSet):
     imei = filters.NumberFilter(field_name="imei")
+    vehicle_id = filters.CharFilter(field_name="vehicle", method='filter_vehicle')
 
     class Meta:
         model = RealTimeDatabase
-        fields = ['imei',]
+        fields = ['imei', 'vehicle_id']
+    
+    def filter_vehicle(self, queryset, name, value):
+        try:
+            vehicle = Vehicle.objects.get(uuid=value)
+            gps = RealTimeDatabase.objects.filter(imei=str(vehicle.device.imei_number)).order_by("-created_at")[:1]
+            return gps
+        except Vehicle.DoesNotExist:
+            return queryset.none()
 
 
 class AlertFilter(filters.FilterSet):
