82ebd8d7775c1dbb08ddb59759b5f6a4a1c7937c
Avinash Mishra <avinashmishra1904@gmail.com>
2023-01-12 03:03:46 +0530

    added https support

--- auth/alert/apis/serializers.py
+++ auth/alert/apis/serializers.py
@@ -82,7 +82,7 @@ class AlertSerializer(serializers.ModelSerializer):
         request = self.context["request"]
         video_url = obj.alert_video_url_shoora
         if video_url:
-            if not request.is_secure():
+            if request.is_secure():
                 video_url = video_url.replace("http", "https")
         return video_url
             
