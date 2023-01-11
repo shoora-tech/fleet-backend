8d1ec779ab81d17779c923d0757dbad4fa880979
Avinash Mishra <avinashmishra1904@gmail.com>
2023-01-12 02:03:11 +0530

    added support for geofence

--- auth/vehicle/tasks.py
+++ auth/vehicle/tasks.py
@@ -14,6 +14,7 @@ from django.conf import settings
 r = redis.Redis(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT, 
+    password=settings.REDIS_PASSWORD,
     db=0,
     charset="utf-8",
     decode_responses=True
